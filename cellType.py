b68f4c21c28eff03ab8bd1a6b45b09b679468c1a
arthurgof <arthur.goffinet@gmail.com>
2022-12-03 18:40:14 +0100

    test

--- cellType.py
+++ cellType.py
@@ -1,19 +1,45 @@
 import numpy as np
 import pandas as pd
+import pickle
 
 
 class CellType:
-    def __init__(self, cluster : pd.DataFrame, marker : list):
-        self.cluster = cluster
+    def __init__(self, cluster_centers : np.ndarray):
+        self.cluster_centers = cluster_centers
         self.cellType = ["Fibroblasts", "Epithelium", "Bcells", "Monocytes",
-                        "Macrophages", "IL17", "T cells", "GamadeltaT"] 
+                        "Macrophages", "IL17", "T cells", "Others"]
         self.cellTypeDict = {}
 
     def getCellType(self, cluster : int):
         return self.cellTypeDict(cluster)
 
     def setCellType(self):
-        pass
-        
+        # Extract from the matlab code and convert to python
+        self.cellTypeDict[self.cluster_centers[:, 7].argmax()] = "Fibroblasts"
+        self.cellTypeDict[self.cluster_centers[:, 13].argmax()] = "Epithelium"
+        self.cellTypeDict[self.cluster_centers[:, 21].argmax()] = "Epithelium"
+        self.cellTypeDict[self.cluster_centers[:, 14].argmax()] = "Bcells"
+        self.cellTypeDict[self.cluster_centers[:, 19].argmax()] = "Monocytes"
+        self.cellTypeDict[self.cluster_centers[:, 33].argmax()] = "Macrophages"
+        tr = self.cluster_centers[:, 42] + self.cluster_centers[:, 7]
+        self.cellTypeDict[tr.argmax()] = "IL17"
+        self.cellTypeDict[self.cluster_centers[:, 46].argmax()] = "T cells"
+
+        lis = list()
+        for i in range(len(self.cluster_centers)):
+            if i not in self.cellTypeDict:
+                lis.append(i)
+
+        for i in lis:
+            self.cellTypeDict[i] = "Others"
+                            
 if __name__ == "__main__":
-    print(len(CellType(None, None).cellType))
+    with open("data/som.pkl", "rb") as f:
+        som = pickle.load(f)
+    cluster_centers = som.cluster_centers_
+    cluster_centers = cluster_centers.reshape(16, 56)
+    cellType = CellType(cluster_centers)
+    cellType.setCellType()
+    # dfn["pred"].apply(lambda x: cellType.cellType[x])
+    for i in range(15):
+        print(cellType.cellTypeDict[i], i)
