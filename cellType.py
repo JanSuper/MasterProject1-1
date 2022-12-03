94e939eb45a64065de728b01fd282b435329259e
arthurgof <arthur.goffinet@gmail.com>
2022-12-03 17:28:14 +0100

    actuak

--- cellType.py
+++ cellType.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pandas as pd
 
 
 class CellType:
-    def __init__(self, cluster : np.ndarray, marker : list):
+    def __init__(self, cluster : pd.DataFrame, marker : list):
         self.cluster = cluster
         self.cellType = ["Fibroblasts", "Epithelium", "Bcells", "Monocytes",
                         "Macrophages", "IL17", "T cells", "GamadeltaT"] 
@@ -11,6 +12,8 @@ class CellType:
     def getCellType(self, cluster : int):
         return self.cellTypeDict(cluster)
 
-
+    def setCellType(self):
+        pass
+        
 if __name__ == "__main__":
     print(len(CellType(None, None).cellType))
