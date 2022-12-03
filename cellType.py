8878dddd92b5b3da632cb542848515b180471edd
arthurgof <arthur.goffinet@gmail.com>
2022-12-03 13:47:45 +0100

    update 2

--- cellType.py
+++ cellType.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class CellType:
-    def __init__(self, cluster : np.ndarray):
+    def __init__(self, cluster : np.ndarray, marker : list):
         self.cluster = cluster
         self.cellType = ["Fibroblasts", "Epithelium", "Bcells", "Monocytes",
                         "Macrophages", "IL17", "T cells", "GamadeltaT"] 
@@ -10,3 +10,7 @@ class CellType:
 
     def getCellType(self, cluster : int):
         return self.cellTypeDict(cluster)
+
+
+if __name__ == "__main__":
+    print("yes")
