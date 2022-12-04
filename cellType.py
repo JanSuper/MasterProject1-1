8786e5e2119b38b5340651c1629b56e7c7ab4087
arthurgof <arthur.goffinet@gmail.com>
2022-12-04 12:16:46 +0100

    cell type proto 1

--- cellType.py
+++ cellType.py
@@ -8,30 +8,50 @@ class CellType:
         self.cluster_centers = cluster_centers
         self.cellType = ["Fibroblasts", "Epithelium", "Bcells", "Monocytes",
                         "Macrophages", "IL17", "T cells", "Others"]
-        self.cellTypeDict = {}
+        self.cellTypeDict = None
 
     def getCellType(self, cluster : int):
+        if self.cellTypeDict is None:
+            self.setCellType()
         return self.cellTypeDict(cluster)
 
     def setCellType(self):
         # Extract from the matlab code and convert to python
-        self.cellTypeDict[self.cluster_centers[:, 7].argmax()] = "Fibroblasts"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 7].argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 13].argmax()] = "Epithelium"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 13].argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 21].argmax()] = "Epithelium"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 21].argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 14].argmax()] = "Bcells"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 14].argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 19].argmax()] = "Monocytes"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 19].argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 33].argmax()] = "Macrophages"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 33].argmax(), axis = 0)
-        tr = self.cluster_centers[:, 42] + self.cluster_centers[:, 7]
-        self.cellTypeDict[tr.argmax()] = "IL17"
-        self.celustercenters = np.delete(self.cluster_centers, tr.argmax(), axis = 0)
-        self.cellTypeDict[self.cluster_centers[:, 46].argmax()] = "T cells"
-        self.celustercenters = np.delete(self.cluster_centers, self.cluster_centers[:, 46].argmax(), axis = 0)
+        self.cellTypeDict = {}
+        save = np.array(self.cluster_centers)
+        mask = np.ones(cluster_centers.shape[0])
+
+        id = self.cluster_centers[:, 7].argmax()
+        self.cellTypeDict[id] = "Fibroblasts"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 13].argmax()
+        self.cellTypeDict[id] = "Epithelium"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 21].argmax()
+        self.cellTypeDict[id] = "Epithelium"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 14].argmax()
+        self.cellTypeDict[id] = "Bcells"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 19].argmax()
+        self.cellTypeDict[id] = "Monocytes"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 33].argmax()
+        self.cellTypeDict[id] = "Macrophages"
+        mask[id] = 0
+        
+        tr = self.cluster_centers[np.where(mask == 1, 1, 0), 42] + self.cluster_centers[np.where(mask == 1, 1, 0), 7]
+        id = tr[np.where(mask == 1, 1, 0)].argmax()
+        self.cellTypeDict[id] = "IL17"
+        mask[id] = 0
+
+        id = self.cluster_centers[np.where(mask == 1, 1, 0), 46].argmax()
+        self.cellTypeDict[id] = "T cells"
 
         lis = list()
         for i in range(len(self.cluster_centers)):
@@ -40,12 +60,14 @@ class CellType:
 
         for i in lis:
             self.cellTypeDict[i] = "Others"
+        self.cluster_centers = save
                             
 if __name__ == "__main__":
     with open("data/som.pkl", "rb") as f:
         som = pickle.load(f)
     cluster_centers = som.cluster_centers_
     cluster_centers = cluster_centers.reshape(16, 56)
+    print(cluster_centers.shape)
     cellType = CellType(cluster_centers)
     cellType.setCellType()
     # dfn["pred"].apply(lambda x: cellType.cellType[x])
